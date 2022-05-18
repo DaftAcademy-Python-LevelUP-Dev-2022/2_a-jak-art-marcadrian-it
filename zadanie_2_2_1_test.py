6d4fdf279646e836388af27c1742cba657d367e7
marcadrian <marcadrian.it@gmail.com>
2022-05-18 19:38:52 +0200

    Update zadanie_2_2_1_test.py

--- zadanie_2_2_1_test.py
+++ zadanie_2_2_1_test.py
@@ -11,10 +11,10 @@ class CodingRoomsUnitTests(unittest.TestCase):
     @sums_of_str_elements_are_equal
     def two_simple_different_ints(self):
         return "1235 4321"
-
+    @sums_of_str_elements_are_equal
     def test_two_simple_ints_are_equal(self):
         assert self.two_simple_ints() == "10 == 10"
-
+    @sums_of_str_elements_are_equal
     def test_two_simple_ints_are_not_equal(self):
         assert self.two_simple_different_ints() == "11 != 10"
 
