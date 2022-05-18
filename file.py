c021b928ba9d94667687a20a79fcaf443175b04d
marcadrian <marcadrian.it@gmail.com>
2022-05-18 19:35:06 +0200

    +sums_of_str_elements_are_equal

--- file.py
+++ file.py
@@ -11,7 +11,34 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def getSum(n):
+        if n<0:
+            n=abs(n)
+            negative=True
+        sum = 0
+        while (n != 0):
+
+            sum = sum + int(n % 10)
+            n = int(n/10)
+        if negative==True:
+            return -abs(sum)
+        else:
+            negative=False
+            return sum
+
+        def wrapper_func(*args, **kwargs):
+            # Do something before the function.
+            nonlocal negative
+            entry = [int(d) for d in re.findall(r'-?\d+', func(*args, **kwargs))]
+            if getSum(entry[0]) == getSum(entry[1]):
+                result=f'{getSum(entry[0])} == {getSum(entry[1])}'
+            else:
+                result=f'{getSum(entry[0])} != {getSum(entry[1])}'
+
+
+            return result
+            # Do something after the function.
+        return wrapper_func
 
 
 def format_output(*required_keys):
