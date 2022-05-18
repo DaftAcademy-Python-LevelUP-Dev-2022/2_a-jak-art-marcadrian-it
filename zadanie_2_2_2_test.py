38d7018d9d3583e7942db60b323237e01bc809a3
marcadrian <marcadrian.it@gmail.com>
2022-05-18 19:39:14 +0200

    Update zadanie_2_2_2_test.py

--- zadanie_2_2_2_test.py
+++ zadanie_2_2_2_test.py
@@ -11,10 +11,10 @@ class CodingRoomsUnitTests(unittest.TestCase):
     @sums_of_str_elements_are_equal
     def two_simple_ints_both_are_negative(self):
         return "-123 -3111"
-
+    @sums_of_str_elements_are_equal
     def test_two_simple_ints_first_is_negative_are_not_equal(self):
         assert self.two_simple_ints_first_is_negative() == "-13 != 13"
-
+    @sums_of_str_elements_are_equal
     def test_two_simple_ints_first_is_negative_are_equal(self):
         assert self.two_simple_ints_both_are_negative() == "-6 == -6"
 
