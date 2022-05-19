05985588995e90e7881f3073dfcba3280d8b4a76
marcadrian <marcadrian.it@gmail.com>
2022-05-19 09:18:36 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,15 +1,14 @@
 import re
 def greeter(func):
 
-    def wrapper_func(*args, **kwargs):
+    def wrapper_func(*args):
         # Do something before the function.
 
-        result="Aloha "+func(*args, **kwargs)
+        result="Aloha "+func(*args)
 
         return result.title()
         # Do something after the function.
     return wrapper_func
-
 
 def sums_of_str_elements_are_equal(func):
     def getSum(n):
@@ -28,10 +27,10 @@ def sums_of_str_elements_are_equal(func):
             negative=False
             return sum
     negative = False
-    def wrapper_func(*args, **kwargs):
+    def wrapper_func(*args):
         # Do something before the function.
         nonlocal negative
-        entry = [int(d) for d in re.findall(r'-?\d+', func(*args, **kwargs))]
+        entry = [int(d) for d in re.findall(r'-?\d+', func(*args))]
         if getSum(entry[0]) == getSum(entry[1]):
             result=f'{getSum(entry[0])} == {getSum(entry[1])}'
         else:
@@ -43,9 +42,44 @@ def sums_of_str_elements_are_equal(func):
     return wrapper_func
 
 
-def format_output(*required_keys):
-    pass
 
+
+
+
+def format_output(*required_keys):
+    def decorator(func):
+        def wrapper_func(*args):
+            entry = func(*args)
+            result = {}
+            for key in required_keys:
+                if '__' in key:
+                    split_keys = key.split('__')
+                    for single_key in split_keys:
+                        if single_key not in entry:
+                            raise ValueError
+
+                    result[key] = ""
+                    for i in range(len(split_keys)):
+                        result[key] = result[key] + entry[split_keys[i]] + " "
+                    result[key] = result[key][:-1]
+
+                else:
+                    if key in entry:
+                        if entry[key] != "":
+                            result[key] = entry[key]
+                        else:
+                            result[key] = "Empty value"
+                    else:
+                        raise ValueError
+
+            return result
+        return wrapper_func
+    return decorator
 
 def add_method_to_instance(klass):
-    pass
+    def outer_wrapper(func):
+        def inner_wrapper(*args, **kwargs):
+            return func()
+        setattr(klass, func.__name__, inner_wrapper)
+        return inner_wrapper
+    return outer_wrapper
